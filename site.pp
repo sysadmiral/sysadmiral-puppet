e359c8638d9b09646dad665dc1b0c35c908be21f
Amo Chumber <amo.chumber@connect-group.com>
2016-04-01 16:59:13 +0100

    Testing deployment stages

--- site.pp
+++ site.pp
@@ -1,1 +1,10 @@
+stage { "pre": before => Stage["main"] }
+
+class someguff {
+  notify {"This is some stuff in the screen"
+    withpath => true,
+  }
+  stage => "pre",
+}
+
 hiera_include('classes')
