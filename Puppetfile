81ba504665ee0fa661ce7c399f9480d89c5c252d
Amo Chumber <amo.chumber@connect-group.com>
2016-04-26 17:04:32 +0100

    add more tools

--- Puppetfile
+++ Puppetfile
@@ -4,6 +4,7 @@ mod 'puppetlabs/stdlib'
 mod 'saz/sudo', '3.1.0'
 mod 'stahnma/epel', '1.2.2'
 mod 'sysadmiral/perconarepo'
+mod 'puppetlabs/apt', '2.2.2'
 #mod 'puppetlabs/accounts', '1.0.0'
 
 # My Modules
--- site/profiles/manifests/personalcomputer/personalcomputerpackages.pp
+++ site/profiles/manifests/personalcomputer/personalcomputerpackages.pp
@@ -3,8 +3,8 @@ class profiles::personalcomputer::personalcomputerpackages {
   #packages that are installed via a puppet module
 
   #packages installed via installer
-  $desiredpkgs = [ 'pwgen', 'dos2unix' ]
-  package { $desiredpkgs: ensure => 'installed' }
+  $desiredpkgs = [ 'pwgen', 'dos2unix', 'curl', 'git']
+  package { $desiredpkgs: ensure => 'latest' }
 
   #packages removed via installer
   $undesiredpkgs = []
