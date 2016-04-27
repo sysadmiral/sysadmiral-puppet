110567726e2c0b0d42f3541b0c8ee2d9f525801a
Amo Chumber <amo.chumber@connect-group.com>
2016-04-27 12:23:50 +0100

    let's do this!

--- site/profiles/manifests/personalcomputer/personalcomputerpackages.pp
+++ site/profiles/manifests/personalcomputer/personalcomputerpackages.pp
@@ -3,7 +3,7 @@ class profiles::personalcomputer::personalcomputerpackages {
   #packages that are installed via a puppet module
 
   #packages installed via installer
-  $desiredpkgs = [ 'pwgen', 'dos2unix', 'curl', 'git', 'nmap', 'subversion', 'tree', 'wget', 'whois', 'at']
+  $desiredpkgs = [ 'pwgen', 'dos2unix', 'curl', 'git', 'nmap', 'subversion', 'tree', 'wget', 'whois', 'at', 'cinnamon']
   package { $desiredpkgs: ensure => 'latest' }
 
   #packages removed via installer
