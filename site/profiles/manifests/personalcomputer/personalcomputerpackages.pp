3cc4e4d1cf3bf310c470ca8b7aadabe026d957f6
Amo Chumber <amo.chumber@connect-group.com>
2016-04-29 16:30:23 +0100

    install virtualbox and os specific packages

--- site/profiles/manifests/personalcomputer/personalcomputerpackages.pp
+++ site/profiles/manifests/personalcomputer/personalcomputerpackages.pp
@@ -1,10 +1,20 @@
 class profiles::personalcomputer::personalcomputerpackages {
 
   #packages that are installed via a puppet module
+  class {'virtualbox':
+    manage_ext_repo => false
+  }
 
   #packages installed via installer
   $desiredpkgs = [ 'pwgen', 'dos2unix', 'curl', 'git', 'nmap', 'subversion', 'tree', 'wget', 'whois', 'at', 'cinnamon', 'lightdm']
   package { $desiredpkgs: ensure => 'latest' }
+
+  $ospackages = $facts['osfamily'] ? {
+    Redhat  => [ 'yum-utils' ],
+    Debian  => [ 'apt-file' ],
+    default => ''
+  }
+  package { $ospackages: ensure => 'latest' }
 
   #packages removed via installer
   $undesiredpkgs = []
