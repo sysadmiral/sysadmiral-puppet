590ca06a909ed975d9ee4a6e45da1120ca67b47b
Amo Chumber <amo.chumber@connect-group.com>
2016-06-02 13:37:19 +0100

    typo in class name

--- site/profiles/manifests/base/baserepos.pp
+++ site/profiles/manifests/base/baserepos.pp
@@ -1,7 +1,7 @@
 # This class will add desired repos as well as pulling in puppet modules that
 # install repos e.g. perconarepo
 
-class profiles::base::repos {
+class profiles::base::baserepos {
   if $facts['osfamily'] == 'Redhat' {
     class { '::epel': stage => 'setup' }
   }
